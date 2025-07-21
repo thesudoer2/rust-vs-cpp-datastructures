d932a5186e4f1caac0b95631a21f280287cefcf1
The Sudoer <thesudoer2@gmail.com>
2025-07-21 14:45:41 +0330

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+CXX_LINK_OPTIONS="-labsl_raw_hash_set -labsl_hashtablez_sampler -labsl_city -labsl_hash -labsl_malloc_internal -labsl_throw_delegate"
+
 mkdir -p bin
-clang++ -std=c++26 benches/$1.cpp -O3 -o bin/cpp-$1
+clang++-20 benches/$1.cpp -std=c++26 -O3 $CXX_LINK_OPTIONS -o bin/cpp-$1
 rustc benches/$1.rs -O -o bin/rust-$1
