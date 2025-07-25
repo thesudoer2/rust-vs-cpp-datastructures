3f4a6308a8c394506c6de0041e9aedfe70ba8dfd
Mohammad Mahdi Nemati Haravani <nemati.mahdi255@gmail.com>
2025-07-25 15:09:19 +0330

    fix absl dependencies

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-CXX_LINK_OPTIONS="-labsl_raw_hash_set -labsl_hashtablez_sampler -labsl_city -labsl_hash -labsl_malloc_internal -labsl_throw_delegate"
+CXX_LINK_OPTIONS="-labsl_raw_hash_set -labsl_hashtablez_sampler -labsl_city -labsl_hash \
+		-labsl_malloc_internal -labsl_throw_delegate -labsl_synchronization -labsl_kernel_timeout_internal \
+		-labsl_base -labsl_time -labsl_time_zone -labsl_stacktrace -labsl_log_severity -labsl_raw_logging_internal \
+		-labsl_debugging_internal -labsl_spinlock_wait -labsl_malloc_internal -labsl_city"
 
 mkdir -p bin
 
